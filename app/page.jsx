f1996a7066fa2326a3cf45e7174683b16c7a9744
Spencer Lott <spklott@gmail.com>
2023-10-14 09:39:25 -0400

    Saving changes

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+import Feed from "@components/feed"
 
 const Home = () => {
   return (
@@ -11,7 +12,7 @@ const Home = () => {
         Promptopia is an open-source AI prompting tool for modern world to discover, create, and share creative prompts
       </p>
 
-      {/*feed*/}
+      <Feed />
     </section>
   )
 }
